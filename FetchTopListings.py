9f0b0e5a4083fb5508be948ea6827d69cf6752b7
ronondex2009 <ronandex2009@gmail.com>
2026-01-11 22:17:53 -0500

    modified:   FetchTopListings.py

--- FetchTopListings.py
+++ FetchTopListings.py
@@ -8,7 +8,7 @@ import time
 def get_marketplace_rows_from_URL(URL):
     """
     Fetches top marketplace entries
-    returns array of tupes of (string item_name, string game_name, int count, float price_starting_at)
+    returns array of tupes of (string item_name, string game_name, int count, float price_starting_at, string link, string datetime)
     """
     # Retry the request if it returns 429 (too many requests) otherwise continue
     wait_time = 30
@@ -29,14 +29,16 @@ def get_marketplace_rows_from_URL(URL):
         print("Request Not OK")
         return []
     soup = BeautifulSoup(marketplace_request.content, "lxml")   # Make BeautifulSoup xml object
-    rows_xml = soup.find_all(class_="market_listing_row")       # Get rows of soup objects for every item in the page
+    rows_xml = soup.find_all(class_="market_listing_row_link")  # Get rows of soup objects for every item in the page
     rows = []
     for row_xml in rows_xml:                                    # Parse the row boxes into data tuples
         rows.append((
             row_xml.find(class_="market_listing_item_name").get_text(),
             row_xml.find(class_="market_listing_game_name").get_text(),
             int(row_xml.find(class_="market_listing_num_listings_qty").get_text().replace(',','')),
-            float(row_xml.find(class_="normal_price").find("span").get_text()[1::])
+            float(row_xml.find(class_="normal_price").find("span").get_text()[1::]),
+            row_xml["href"]
+            datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%dT%h:%m:%s")
         ))
     return rows
 
@@ -48,22 +50,21 @@ def get_marketplace_rows(numpages):
         _rows = get_marketplace_rows_from_URL(URL)
         print(f"{URL} fetched {len(_rows)} results.")
         rows.extend(_rows)
+        time.sleep(1)
     print(f"Fetched a total of {len(rows)} rows")
     return rows
 
 def write_to_top_results_database(rows):
-    # Add datetime object to the data tuples.
-    rows = [(row[0], row[1], row[2], row[3], datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%dT%h:%m:%s")) for row in rows]
     # Create connection, check database schema and create it if its not there, and then write all tuples to the database.
     conn = sqlite3.connect("./SteamMarketplace.db")
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS top_listings (item_name NVARCHAR(64), game_name NVARCHAR(64) NOT NULL, quantity INTEGER NOT NULL, normal_price DECIMAL(9, 2), during DATETIME NOT NULL);")
-    cursor.executemany("INSERT INTO top_listings (item_name, game_name, quantity, normal_price, during) VALUES (?, ?, ?, ?, ?)", rows)
+    cursor.execute("CREATE TABLE IF NOT EXISTS top_listings (item_name NVARCHAR(64) NOT NULL, game_name NVARCHAR(64) , quantity INTEGER, normal_price DECIMAL(9, 2), link NVARCHAR(256), during DATETIME);")
+    cursor.executemany("INSERT INTO top_listings (item_name, game_name, quantity, normal_price, link, during) VALUES (?, ?, ?, ?, ?, ?)", rows)
     conn.commit()
 
 if __name__=="__main__":
     # Get tuples
-    rows = get_marketplace_rows(50)
+    rows = get_marketplace_rows(100)
     # Print tuples
     print(f"{"Item Name":<64}{"Game Name":<64}{"Normal Price":<12}{"Quantity":<16}")
     for row in rows:
